17d0cd283d77b9f3aca9e507e4215da7254514ff
Deeclun <fisher.declan@yahoo.co.uk>
2016-09-23 12:45:55 +0100

    Add files via upload

---
+++ Computer Science Project/Crypto-Helper/Crypto-Helper/chHome.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Crypto_Helper
+{
+    /// <summary>
+    /// Interaction logic for chHome.xaml
+    /// </summary>
+    public partial class chHome : Window
+    {
+        public chHome()
+        {
+            InitializeComponent();
+        }
+
+
+        #region Close & Minimize
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Environment.Exit(1);
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        #endregion
+    }
+}
