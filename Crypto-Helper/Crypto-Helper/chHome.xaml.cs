a78bd624238da25c4d32689a43f6c844dd89b9ca
Deeclun <fisher.declan@yahoo.co.uk>
2016-09-25 12:57:17 +0100

    Form changes
    -

--- Crypto-Helper/Crypto-Helper/chHome.xaml.cs
+++ Crypto-Helper/Crypto-Helper/chHome.xaml.cs
@@ -7,32 +7,23 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Crypto_Helper
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for chHome.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class chHome : Window
     {
-        public MainWindow()
+        public chHome()
         {
             InitializeComponent();
         }
 
-        #region Moveable Form
-        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
-        {
-            base.OnMouseLeftButtonDown(e);
-            DragMove();
-        }
-        #endregion
 
         #region Close & Minimize
         private void btnClose_Click(object sender, RoutedEventArgs e)
